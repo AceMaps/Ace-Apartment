d2bcd20c716cc46f68449d7d8cd154aa642e8d60
Ace <104071730+AceMaps@users.noreply.github.com>
2022-10-31 23:47:17 +0100

    Add files via upload

---
+++ ace_apartment_nopoolballs/fxmanifest.lua
@@ -0,0 +1,17 @@
+-- For support join my discord: https://discord.gg/uZRXDbwpKA
+
+author "NaknoBaboch#5776"
+description "Apartment"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+
+files {
+
+"interiorproxies.meta"
+}
+
+this_is_a_map 'yes'
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
